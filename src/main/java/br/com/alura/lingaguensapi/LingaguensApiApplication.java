94434b9db3b1120a1ef361f22d8c5a69981be667
Juan Cassiano <juancassiano@hotmail.com>
2022-07-23 17:06:16 -0300

    :tada: first spring commit for api of languagens ranking

---
+++ src/main/java/br/com/alura/lingaguensapi/LingaguensApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.lingaguensapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingaguensApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingaguensApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/lingaguensapi/LingaguensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.lingaguensapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingaguensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
